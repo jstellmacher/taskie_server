96b9343341f3f5bc503b4ebe1a1f320711d586bf
jai <jaijai.stell@gmail.com>
2024-05-23 20:26:42 -0700

    Initial

---
+++ src/main/java/com/taskie/taskie_server/TaskieServerApplication.java
@@ -0,0 +1,13 @@
+package com.taskie.taskie_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskieServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskieServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskie/taskie_server/TaskieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskie.taskie_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskieServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
